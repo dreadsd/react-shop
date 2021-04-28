f4af2a1265196723f9236cad1c93527fddb050b3
Erik Tejeda <etrq@protonmail.com>
2021-04-28 10:59:14 -0500

    Add items to cart

--- src/PubSub.js
+++ src/PubSub.js
@@ -4,6 +4,16 @@ const PubSub = {
     this.events[en] = this.events[en] || [];
     this.events[en].push(fn);
   },
+  unsubscribe(en, fn) {
+    if (this.events[en]) {
+      for (let i = 0; i < this.events[en].length; i += 1) {
+        if (this.events[en][i] === fn) {
+          this.events[en].splice(i, 1);
+          break;
+        }
+      }
+    }
+  },
   publish(en, data) {
     if (this.events[en]) {
       this.events[en].forEach((fn) => fn(data));
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PubSub from '../PubSub';
 
 export default function Cart() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState({});
   const [isExpanded, setIsExpanded] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   const expandCart = () => {
     const list = document.getElementById('item-cart-items');
@@ -11,26 +12,43 @@ export default function Cart() {
     Array.from(list.children).forEach((element) => element.classList[method]('expanded'));
     setIsExpanded((val) => !val);
   };
-  const addToCart = (obj) => {
-    setItems([...items, obj]);
+  const addToCart = (data) => {
+    setCounter((v) => {
+      const received = { ...data };
+      setItems((v1) => {
+        const oldItems = { ...v1 };
+        received.id = v;
+        oldItems[data.id] = (oldItems[data.id] || []).concat(received);
+        return oldItems;
+      });
+      return v + 1;
+    });
+  };
+  const parseItems = () => {
+    const parsed = [];
+    Object.keys(items).forEach((key) => {
+      items[key].forEach((item) => {
+        parsed.push(
+          <div id={item.id} key={item.id} className={isExpanded ? 'expanded' : null}>{item.name}</div>,
+        );
+      });
+    });
+    return parsed;
   };
 
-  PubSub.subscribe('add to cart', addToCart);
+  useEffect(() => {
+    PubSub.subscribe('add to cart', addToCart);
+    return PubSub.unsubscribe.bind(PubSub, 'add to cart', addToCart);
+  }, []);
 
   return (
     <div id='cart'>
+      <div>{counter}</div>
       <div id='item-cart' onClick={expandCart}>
         <i className='gg-shopping-cart' />
       </div>
       <div id='item-cart-items'>
-        {items.map((item, idx) => (
-          <div
-            key={idx}
-            className={isExpanded ? 'expanded' : null}
-          >
-            {item.name}
-          </div>
-        ))}
+        {parseItems()}
       </div>
     </div>
   );
